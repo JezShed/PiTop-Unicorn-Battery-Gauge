3aab6416433e6170c878a3af49dce23e903faec3
JezShed <jez@highpeak100.co.uk>
2016-05-01 10:40:30 +0100

    Added files via upload
    Tidied code into functions.

--- bat.py
+++ bat.py
@@ -3,8 +3,9 @@
 ##########################################################
 ## pi-top Battery level gauge using Pimoroni's Unicorn  ##
 ## Hat by calling 'pt-battery' and capturing its output.##
-## Samples every 10 seconds to keep CPU usage down.     ##
+## Also displays CAPS-LOCK status                       ##
 ## Example by Jez - @JezShed                            ##
+## Version 1.2, 1st May 2016                            ##
 ##########################################################
 
 import unicornhat, time, subprocess
@@ -20,26 +21,18 @@ def drawbattery():    #Draw the naked battery
     unicornhat.show()
 
 
-##############################
-## Main program starts here ##
-##############################
-unicornhat.rotation(0)
-unicornhat.clear()  # Clear the display
-unicornhat.brightness(0.5) # Globalunicorn brightness
-drawbattery()       # Draw the battery
-unicornhat.show()
-time.sleep(0.5)
-bat=100             # Default is an empty battery if no response obtained from pt-battery
-startanim=1         # Start-up anim only on the first run
-while True:
+def getbatterystatus():
     # Issue 'pt-battery' command and capture response
     p = subprocess.Popen(["pt-battery"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     answer, err = p.communicate()
+    return(answer)
 
+
+def chargecapacity(batterystatus):
     # Now search the returned string for the capacity figure
-    print(answer)
-    str1=str(answer)
-    str2="Capacity"
+    bat=-1 # Rogue value of -1 if no usable response from pt-battery
+    str1=str(batterystatus)
+    str2="Capacity" # Search for this word
     position=str1.find(str2)+9
     capacity=(str1[position:position+4])
     if capacity[3]=="%":
@@ -48,7 +41,10 @@ while True:
         bat=int(capacity)
     except ValueError:
         print("pt-battery didn't return a capacity figure, probably busy, I'll try again.")
+    return(bat)
 
+
+def showcapacity(bat,startanim):
     # Plot the battery level on the Unicorn Hat
     b=0
     for y in range(0,6):
@@ -89,31 +85,66 @@ while True:
         unicornhat.set_pixel(4,6-y,r,g,b)
         unicornhat.set_pixel(5,6-y,r,g,b)
         unicornhat.set_pixel(6,6-y,r,g,b)
-        
-    startanim=0
-    # Display little "+" symbol if we are charging
-    if str1.find("Charging")>0:
-        chargesymbol=255
+
+
+def showchargesymbol(batterystatus):
+    if batterystatus.find("Charging")>0:
+        r=255
     else:
-        chargesymbol=0
-    unicornhat.set_pixel(1,0,chargesymbol,0,0)
-    unicornhat.set_pixel(0,1,chargesymbol,0,0)
-    unicornhat.set_pixel(1,1,chargesymbol,0,0)
-    unicornhat.set_pixel(2,1,chargesymbol,0,0)
-    unicornhat.set_pixel(1,2,chargesymbol,0,0)
-        
+        r=0
+    unicornhat.set_pixel(1,0,r,0,0)
+    unicornhat.set_pixel(0,1,r,0,0)
+    unicornhat.set_pixel(1,1,r,0,0)
+    unicornhat.set_pixel(2,1,r,0,0)
+    unicornhat.set_pixel(1,2,r,0,0)
+
+
+def capslockstatus():
+    p = subprocess.Popen(["xset q | grep LED"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    answer, err = p.communicate()
+    led=int(str(answer)[67:68]) # Extract 67th character from string
+    return(led)                 # =1 for CAPS LOCK ON
+
+
+def showcapslock(led):
+    if led==1:
+        r=255
+        g=255
+        b=255
+    else:
+        r=0
+        g=0
+        b=0
+    unicornhat.set_pixel(0,5,r,g,b)
+    unicornhat.set_pixel(0,6,r,g,b)
+    unicornhat.set_pixel(0,7,r,g,b)
+    unicornhat.set_pixel(1,5,r,g,b)
+    unicornhat.set_pixel(1,7,r,g,b)
+
+
+##############################
+## Main program starts here ##
+##############################
+unicornhat.rotation(0)
+unicornhat.clear()          # Clear the display
+unicornhat.brightness(0.5)  # Globalunicorn brightness
+drawbattery()               # Draw the battery
+unicornhat.show()
+time.sleep(0.5)
+startanim=1                 # Start-up anim only on the first run
+while True:
+    batterystatus=str(getbatterystatus())
+    print(batterystatus)
+    bat=chargecapacity(batterystatus)
+    if bat!=-1:
+        showcapacity(bat,startanim)
+    startanim=0
+    
+    # Display little "+" symbol if we are charging
+    showchargesymbol(batterystatus)
+
+    # Checks CAPS LOCK 10 times (once every 0.5 Secs) then carries on
     for y in range(0,10):
-        p = subprocess.Popen(["xset q | grep LED"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        answer, err = p.communicate()
-        str1=str(answer)
-        if str1[67:68]=='1':
-            capsymbol=255
-        else:
-            capsymbol=0
-        unicornhat.set_pixel(0,5,capsymbol,capsymbol,capsymbol)
-        unicornhat.set_pixel(0,6,capsymbol,capsymbol,capsymbol)
-        unicornhat.set_pixel(0,7,capsymbol,capsymbol,capsymbol)
-        unicornhat.set_pixel(1,5,capsymbol,capsymbol,capsymbol)
-        unicornhat.set_pixel(1,7,capsymbol,capsymbol,capsymbol)
+        showcapslock(capslockstatus())
         unicornhat.show()
-        time.sleep(0.5)          # Sleep most of the time
+        time.sleep(0.5)     # Sleep most of the time
